1206e33156a0720fe39b8e96a1f362efec81e32b
Chen, Weihang (Bisnode) <weihang.chen@bisnode.com>
2015-11-05 15:51:41 +0100

    impression count

---
+++ Domain/TobeRemove/RemoveMe.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.TobeRemove
+{
+    public class RemoveMe
+    {
+        public Count GetCount(ICollection<WhatEver> data)
+        {
+            var impressionCount = data.Where(x => x == WhatEver.impression).Count();
+            return new Count { ImpressionCount = impressionCount, ClickCount = (data.Count() - impressionCount) };
+        }
+    }
+
+    public enum WhatEver
+    {
+        impression, click
+    }
+
+    public class Count
+    {
+        public double ImpressionCount { get; set; }
+        public double ClickCount { get; set; }
+    }
+}
